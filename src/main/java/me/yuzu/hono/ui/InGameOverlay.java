0a89c78db1070c1394b4771a15b0962b04ab34d0
yuzu-krs <yuzuto.poi@gmail.com>
2025-11-03 12:46:03 +0900

    rainbow-ui-modlist

--- src/main/java/me/yuzu/hono/ui/InGameOverlay.java
+++ src/main/java/me/yuzu/hono/ui/InGameOverlay.java
@@ -1,7 +1,11 @@
 package me.yuzu.hono.ui;
 
+import java.util.List;
+
 import com.mojang.blaze3d.systems.RenderSystem;
 
+import me.yuzu.hono.Hono;
+import me.yuzu.hono.util.RainbowUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 
@@ -39,9 +43,40 @@ public class InGameOverlay {
 			int y2=y1+totalHeight;
 			guiGraphics.fill(x1-5, y1-3, x2, y2,0x99000000);
 			
+			
+			renderEnabledMods(guiGraphics);
+			
 			RenderSystem.disableBlend();
 			
+		}
+	}
+	
+	private void renderEnabledMods(GuiGraphics guiGraphics) {
+		List<me.yuzu.hono.module.Module> enabledMods=Hono.instance.modManager.getEnabledModules();
+		
+		int screenWidth=mc.getWindow().getGuiScaledWidth();
+		int yOffset=5;
+		
+		for(int i=0;i<enabledMods.size();i++) {
+			me.yuzu.hono.module.Module mod=enabledMods.get(i);
+			String modName=mod.getName();
+			int modNameWidth=mc.font.width(modName);
+			
+			int rainbowColor=RainbowUtil.getRainbowColor(i*0.1f);
+			
+			guiGraphics.drawString(mc.font, "- " + modName,screenWidth-50,yOffset,rainbowColor);
+
+			yOffset += mc.font.lineHeight+2;
 			
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
